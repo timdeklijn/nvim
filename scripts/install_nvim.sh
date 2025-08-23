29043984748f192df34209cf29d17520c8af7311
Tim de Klijn <timdeklijn@gmail.com>
2025-08-23 13:10:05 +0200

    fix typo in nvim install

--- scripts/install_nvim.sh
+++ scripts/install_nvim.sh
@@ -4,4 +4,5 @@ curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim-linux-x8
 sudo rm -rf /opt/nvim
 sudo tar -C /opt -xzf nvim-linux-x86_64.tar.gz
 rm nvim-linux-x86_64.tar.gz
-echo $(export PATH="$PATH:/opt/nvim-linux-x86_64/bin") >>~/.bashrc
+
+echo 'export PATH="$PATH:/opt/nvim-linux-x86_64/bin"' >>~/.bashrc
